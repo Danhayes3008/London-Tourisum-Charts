75adbb08ef8b3b6eb25a698d3446168b321f24e4
Daniel Hayes <danhayes3008@outlook.com>
2019-02-19 16:07:57 +0000

    added the selector to the web page

--- Static/JS/graph.js
+++ Static/JS/graph.js
@@ -1,1 +1,22 @@
-test
+queue()
+    .defer(d3.csv, "Static/Data/international-visitors-london.csv")
+    .await(makeGraphs);
+    
+function makeGraphs(error, londonData){
+    var ndx = crossfilter(londonData);
+    
+    show_selector(ndx);
+    
+    dc.renderAll();
+}
+
+
+function show_selector(ndx){
+    
+    var dim = ndx.dimension(dc.pluck('market'));
+    var group = dim.group();
+    
+    dc.selectMenu('#selectMenu')
+        .dimension(dim)
+        .group(group);
+}
