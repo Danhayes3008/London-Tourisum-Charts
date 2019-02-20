3c639993dce829db6c48ed460804aa9b776db9fc
Daniel Hayes <danhayes3008@outlook.com>
2019-02-20 14:57:50 +0000

    added a pie chart for method of entry and visits per country(not complete) some code commented out whilst i work on it.

--- Static/JS/graph.js
+++ Static/JS/graph.js
@@ -6,7 +6,10 @@ function makeGraphs(error, londonData){
     var ndx = crossfilter(londonData);
     
     show_selector(ndx);
+    // show_total_spent(ndx, "#spent");
     show_reason_for_visit(ndx);
+    show_method_of_arrival(ndx);
+     show_country_of_origin(ndx);
     
     dc.renderAll();
 }
@@ -21,6 +24,12 @@ function show_selector(ndx){
         .dimension(dim)
         .group(group);
 }
+
+// function show_total_spent(ndx, spent, element){
+//     var totalSpent = ndx.groupAll()
+//     dc.numberDisplay()
+        
+// }
 
 function show_reason_for_visit(ndx){
     var dim = ndx.dimension(dc.pluck('purpose'));
@@ -37,4 +46,41 @@ function show_reason_for_visit(ndx){
         .xUnits(dc.units.ordinal)
         .xAxisLabel("method of transport")
         .yAxis().ticks(5);
+}
+
+function show_method_of_arrival(ndx) {
+    var typeColors = d3.scale.ordinal()
+        .domain(["Tunnel", "Sea", "Air"])
+        .range(["#0481BB", "#048AC9", "#069CE3" ]);
+    var arrivalDim = ndx.dimension(dc.pluck("mode"));
+    var methodOfArrival = arrivalDim.group();
+
+    dc.pieChart("#arrival")
+        .height(250)
+        .radius(100)
+        .transitionDuration(500)
+        .dimension(arrivalDim)
+        .group(methodOfArrival)
+        .colorAccessor(function(d) { return d.key[0]; })
+        .colors(typeColors);
+}
+
+function show_country_of_origin(ndx){
+    var typeColors = d3.scale.ordinal()
+        .domain(["Argentina", "Australia", "Austria", "Bahrain", "Belgium", "Brazil", "Bulgaria", "Canada", "Chile", 
+        "China", "Czech Republic", "Denmark", "Egypt", "Finland", "France", "Germany", "Greece", "Hong Kong",
+        "Hungary", "Iceland", "India", "Indonesia", "Irish Republic", "Israel", "Italy", "Japan", "Kenya",
+        "Kuwait", "Luxembourg", "Malaysia", "Mexico", "Netherlands", "New Zealand", "Nigeria", "Norway", "Omen"])
+        .range(["#0481BB", "#048AC9", "#069CE3"]);
+    var countryDim = ndx.dimension(dc.pluck("market"));
+    var countryOfOrigin = countryDim.group();
+    
+    dc.pieChart("#country")
+        .height(250)
+        .radius(100)
+        .transitionDuration(500)
+        .dimension(countryDim)
+        .group(countryOfOrigin)
+        .colorAccessor(function(d){ return d.key[0]; })
+        .colors(typeColors);
 }
