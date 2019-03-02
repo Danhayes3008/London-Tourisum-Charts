1fab61efaa5abbda55edbf07da62c3c390528122
Daniel Hayes <danhayes3008@outlook.com>
2019-03-02 16:32:29 +0000

    re-added the visits per country chart and did fixed some errors

--- Static/JS/graph.js
+++ Static/JS/graph.js
@@ -11,9 +11,9 @@ function makeGraphs(error, londonData){
         d.reason = parseInt(d.purpose);
         d.nights = parseFloat(d.nights);
         d.year = parseInt(d.year);
-        d.spend = parseFloat(d.spend);
+        d.spend = parseInt(d.spend);
         d.visits = parseFloat(d.visits);
-        d.quarter = parseInt(d.quarter);
+        d.quarter = parseFloat(d.quarter);
         d.sample = parseInt(d.sample);
         d.area = parseInt(d.area);
 
@@ -27,6 +27,7 @@ function makeGraphs(error, londonData){
     show_visit_per_year(ndx);
     nights_stayed_per_country(ndx);
     visits_per_country(ndx);
+    visits_per_country1(ndx);
     visits_per_year(ndx);
 
     dc.renderAll();
@@ -152,6 +153,62 @@ function nights_stayed_per_country(ndx){
         .xAxis().ticks(10);
 }
 
+function visits_per_country1(ndx) {
+        
+        
+        var visit_dim = ndx.dimension(dc.pluck('visits'));
+        
+        var min_visited = visit_dim.bottom(1)[0].visits;
+        var max_visited = visit_dim.top(1)[0].visits;
+        
+        var london_dim = ndx.dimension(function (d) {
+            return [d.visits, d.spend, d.market, d.year, d.quarter, d.purpose];
+        });
+        
+        var tradeColors = d3.scale.ordinal()
+            .domain(["q1", "q2", "q3", "q4"])
+            .range(["red", "green", "blue", "purple", "yellow"]);
+
+
+        var london_group = london_dim.group();
+        
+        var subChart = function(c) {
+            return dc.scatterPlot(c)
+                        .symbolSize(4)
+                        .highlightedSize(10);
+        };
+
+        
+        var chart = dc.seriesChart("#scatter2");
+        chart
+        .width(770)
+        .height(480)
+        .chart(subChart)
+        .x(d3.time.scale().domain([min_visited, max_visited]))
+        .brushOn(false)
+        .clipPadding(20)
+        .yAxisLabel("year")
+        .xAxisLabel("visits")
+        .elasticY(true)
+        .dimension(london_dim)
+        .group(london_group)
+        .mouseZoomable(true)
+        .shareTitle(true) // allow default scatter title to work
+        .seriesAccessor(function(d) {return d.key[5];})
+        .keyAccessor(function(d) {return d.key[0];})
+        .valueAccessor(function(d) {return +d.key[3];})
+        .colorAccessor(function (d) {
+            return d.key[3];
+        })
+        .colors(tradeColors)
+
+        .legend(dc.legend().x(700).y(50).itemHeight(13).gap(5).horizontal(1).legendWidth(70).itemWidth(70));
+
+        chart.margins().left += 40;
+        chart.margins().bottom += 20;
+        chart.margins().right = 150;
+        dc.renderAll();
+    }
 
 function visits_per_country(ndx){
     var countryDim = ndx.dimension(dc.pluck('market'));
